d073134f401fa2cfb78fe1d8b40485d4f8f44119
kongyi <jerrykongyi1997@gmail.com>
2020-08-22 17:23:44 +0800

    优化代码

--- src/test.js
+++ src/test.js
@@ -1,53 +1,65 @@
 import string from "./css.js";
 
-let n = 1;
-demo2.innerHTML = string.substr(0, n);
-demo.innerText = string.substr(0, n);
-
-let time = 100;
-
-const run = () => {
-  n += 1;
-  if (n > string.length) {
-    window.clearInterval(id);
-    return;
-  }
-  demo2.innerHTML = string.substr(0, n);
-  demo.innerText = string.substr(0, n);
-  demo.scrollTop = demo.scrollHeight;
+const player = {
+  n: 1,
+  time: 100,
+  id: undefined,
+  ui: {
+    demo: document.querySelector("#demo"),
+    demo2: document.querySelector("#demo2"),
+  },
+  events: {
+    "#btnPause": "pause",
+    "#btnPlay": "play",
+    "#btnSlow": "slow",
+    "#btnNormal": "normal",
+    "#btnFast": "fast",
+  },
+  init: () => {
+    player.ui.demo2.innerHTML = string.substr(0, player.n);
+    player.ui.demo.innerText = string.substr(0, player.n);
+    player.bindEvents();
+    player.play();
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      if (player.events.hasOwnProperty(key)) {
+        const value = player.events[key];
+        document.querySelector(key).onclick = player[value];
+      }
+    }
+  },
+  run: () => {
+    player.n += 1;
+    if (player.n > string.length) {
+      window.clearInterval(player.id);
+      return;
+    }
+    player.ui.demo2.innerHTML = string.substr(0, player.n);
+    player.ui.demo.innerText = string.substr(0, player.n);
+    player.ui.demo.scrollTop = demo.scrollHeight;
+  },
+  play: () => {
+    player.id = setInterval(player.run, player.time);
+  },
+  pause: () => {
+    window.clearInterval(player.id);
+  },
+  slow: () => {
+    player.pause();
+    player.time = 100;
+    player.play();
+  },
+  normal: () => {
+    player.pause();
+    player.time = 50;
+    player.play();
+  },
+  fast: () => {
+    player.pause();
+    player.time = 0;
+    player.play();
+  },
 };
 
-let id = setInterval(() => {
-  run();
-}, time);
-
-btnPause.onclick = () => {
-  window.clearInterval(id);
-};
-
-btnPlay.onclick = () => {
-  id = setInterval(() => {
-    run();
-  }, time);
-};
-btnSlow.onclick = () => {
-  window.clearInterval(id);
-  time = 200;
-  id = setInterval(() => {
-    run();
-  }, time);
-};
-btnNormal.onclick = () => {
-  window.clearInterval(id);
-  time = 100;
-  id = setInterval(() => {
-    run();
-  }, time);
-};
-btnFast.onclick = () => {
-  window.clearInterval(id);
-  time = 10;
-  id = setInterval(() => {
-    run();
-  }, time);
-};
+player.init();
